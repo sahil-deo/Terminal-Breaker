3192a430a4dee154c0e87d08e1a1592fa6b354d7
Sahil <sahildeo616@gmail.com>
2026-08-23 18:58:00 +0530

    better text edit

--- Scripts/text_edit.gd
+++ Scripts/text_edit.gd
@@ -1,7 +1,25 @@
 extends TextEdit
 
+var _restoring := false
+
+func _ready() -> void:
+	caret_changed.connect(_on_caret_changed)
+
 func _gui_input(event: InputEvent) -> void:
-	if event is InputEventKey and event.pressed and event.keycode == KEY_BACKSPACE:
-		var caret_col := get_caret_column()
-		if caret_col == 0:
-			accept_event()  # consume it, prevents merging with previous line
+	if event is InputEventKey and event.pressed:
+		match event.keycode:
+			KEY_BACKSPACE:
+				if get_caret_column() == 0:
+					accept_event()
+			KEY_UP, KEY_DOWN, KEY_PAGEUP, KEY_PAGEDOWN:
+				accept_event()
+
+func _on_caret_changed() -> void:
+	if _restoring:
+		return
+	var last_line := get_line_count() - 1
+	if get_caret_line() != last_line:
+		_restoring = true
+		set_caret_line(last_line)
+		set_caret_column(get_line(last_line).length())
+		_restoring = false
