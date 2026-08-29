58216f894a00b4051491cc23bb67c9c1504e576b
SanjanaDeo <104987269+SanjanaDeo@users.noreply.github.com>
2026-08-29 17:39:34 +0530

    fixing

--- Scripts/game_controller.gd
+++ Scripts/game_controller.gd
@@ -91,9 +91,3 @@ func getChatWindow() -> RichTextLabel:
 
 func addProgress(progress: float):
 	progressBar.value += progress
-
-func getProgress():
-	print("Got progress")
-
-func gameOver():
-	print("Game Over")
